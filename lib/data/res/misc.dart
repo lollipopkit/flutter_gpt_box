abd5ca27385a2ea4b87085bee2a5f05fc2f2c8bd
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2024-09-12 20:50:14 +0800

    chore: cleanup

--- lib/data/res/misc.dart
+++ lib/data/res/misc.dart
@@ -1,6 +1,3 @@
 abstract final class Miscs {
   static const bakFileName = 'gptbox_bak.json';
-
-  static const annoKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzE4NzI2NDAwLAogICJleHAiOiAxODc2NDkyODAwCn0.FXx4qCETGSLEdJsX1PKyrDg19-4JuvMpiLFcClr4PQ8';
 }
