31c541da881383f76b4b024479b2bcc90e6a7931
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2025-03-25 16:35:17 +0800

    fix & opt.

--- lib/core/ext/file.dart
+++ lib/core/ext/file.dart
@@ -9,7 +9,13 @@ extension FileX on File {
     final format = await mimeType;
     final bytes = await readAsBytes();
     if (format == null) {
-      return compute(utf8.decode, bytes);
+      if (bytes.length > 1024 * 5) {
+        // Offload large arrays to a separate isolate.
+        return await compute(utf8.decode, bytes);
+      } else {
+        // For smaller arrays, decode directly to avoid overhead.
+        return utf8.decode(bytes);
+      }
     }
     return 'data:$format;base64,${base64Encode(bytes)}';
   }
